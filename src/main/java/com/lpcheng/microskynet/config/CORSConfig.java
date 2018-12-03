825c796cf70fb1168c0330782e806dfa3577ab68
Administrator <15077774211@163.com>
2018-12-03 18:47:28 +0800

    kafka listener

--- src/main/java/com/lpcheng/microskynet/config/CORSConfig.java
+++ src/main/java/com/lpcheng/microskynet/config/CORSConfig.java
@@ -7,8 +7,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CORSConfiguration {
-
+public class CORSConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*");
